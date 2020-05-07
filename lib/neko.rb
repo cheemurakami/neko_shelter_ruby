1b1413484b6fca2264a64b7a9feff2e4b0b89e23
Chisato Murakami <kiwipom1031@gmail.com>
2020-05-07 15:32:21 -0700

    add neko treat method

--- lib/neko.rb
+++ lib/neko.rb
@@ -1,8 +1,28 @@
 class Neko
-  def initialize(name)
+  def initialize(name, color)
     @stored_name = name
+    @stored_color = color
+    @stored_onaka_level = 0
   end
+
   def name
     @stored_name
+  end
+
+  def color
+    @stored_color
+  end
+
+  def treat
+    @stored_onaka_level += 1
+    if(@stored_onaka_level < 5)
+      "OKO=^-_-^="
+    else
+      "FULL=^-_-^="
+    end
+  end
+
+  def onaka_level
+    @stored_onaka_level
   end
 end
--- spec/neko_spec.rb
+++ spec/neko_spec.rb
@@ -4,11 +4,32 @@ require ('neko')
 
 describe('neko') do
   it("is 2")do
-  expect(2).to eq(2)
+    expect(2).to eq(2)
   end
-  it("gives neko a name")do
-  neko = Neko.new("kiwi")
-  expect(neko.name).to eq("kiwi")
+  it("gives neko a name") do
+    neko = Neko.new("kiwi", "black")
+    expect(neko.name).to eq("kiwi")
   end
+  it("gives neko a color") do
+    neko = Neko.new("kiwi", "black")
+    expect(neko.color).to eq("black")
+  end
+  it("feeds neko a treat") do
+    neko = Neko.new("kiwi", "black")
+    expect(neko.treat).to eq("=^-_-^=")
+  end
+  it("adds onaka level when neko is fed") do
+    neko = Neko.new("kiwi", "black")
+    neko.treat
+    neko.treat
+    neko.treat
+    neko.treat
+    neko.treat
+    neko.treat
+    expect(neko.onaka_level).to eq(6)
+    expect(neko.treat).to eq("=^-_-^=")
+  end
+
+
 end
 

2bccc05a928ae2685e349ca148f33a6e5c5fa135
Chisato Murakami <kiwipom1031@gmail.com>
2020-05-07 21:19:47 -0700

    add loop in main.rb

--- main.rb
+++ main.rb
@@ -9,33 +9,42 @@ your_cat_color = gets.chomp
 neko = Neko.new(your_cat, your_cat_color)
 puts "Welcome #{neko.color} cat #{neko.stored_name}!!"
 
-puts "Enter a number"
-puts "1. Give food"
-puts "2. Give treat"
-puts "3. Play"
-puts "4. Sleep"
-puts "5. Clean"
-puts "6. Quit"
 
-choice = gets.chomp
-if choice == "1"
-  puts neko.give_food
-elsif choice == "2"
-  puts "Choose your treats"
-  puts "1. karikari"
-  puts "2. wet food"
-  treat_choice = gets.chomp
-  if treat_choice == "1"
-    puts neko.treat("karikari")
-  else
-    puts neko.treat("wet food")
+choice = ""
+
+until choice == "6" do
+  puts ""
+  puts "Enter a number"
+  puts "1. Give food"
+  puts "2. Give treat"
+  puts "3. Play"
+  puts "4. Sleep"
+  puts "5. Clean"
+  puts "6. Quit"
+  puts ""
+
+  choice = gets.chomp
+  puts ""
+  if choice == "1"
+    puts neko.give_food
+  elsif choice == "2"
+    puts "Choose your treats"
+    puts "1. karikari"
+    puts "2. wet food"
+    treat_choice = gets.chomp
+    if treat_choice == "1"
+      puts neko.treat("karikari")
+    else
+      puts neko.treat("wet food")
+    end
+  elsif choice == "3"
+    puts neko.play
+  elsif choice == "4"
+    puts neko.neru
+  elsif choice == "5"
+    puts neko.clean
+  elsif choice == "6"
+    puts "bye bye"
   end
-elsif choice == "3"
-  puts neko.play
-elsif choice == "4"
-  puts neko.neru
-elsif choice == "5"
-  puts neko.clean
-elsif choice == "6"
-  puts "bye bye"
 end
+
